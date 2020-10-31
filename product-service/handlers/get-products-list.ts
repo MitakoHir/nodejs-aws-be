53d89998b5a88db39aff9773d96b39cf658b8ed5
MitakoHir <mitakohir@gmail.com>
2020-10-31 07:43:01 +0300

    cors enabled for both end-points, additionally headers provided to the getProductsList function

--- product-service/handlers/get-products-list.ts
+++ product-service/handlers/get-products-list.ts
@@ -3,14 +3,22 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import productList from '../mock-data/productList.json';
 
 export const getProductsList: APIGatewayProxyHandler = async (_event, _context) => {
+  const headers = {
+      "Access-Control-Allow-Headers" : "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+  };
+
   try {
     return {
       statusCode: 200,
+      headers,
       body: JSON.stringify(productList)
     }
   } catch (e) {
     return {
       statusCode: 500,
+      headers,
       body: `There is an unexpected error ${ JSON.stringify(e) }`
     }
   }
