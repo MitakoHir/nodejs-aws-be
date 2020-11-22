70366e03faaf8f1cc654e7d415bb3122a0b3b4b1
MitakoHir <mitakohir@gmail.com>
2020-11-22 16:43:40 +0300

    aws sdk moved to dev-dependencies

--- product-service/handlers/get-products-by-id.ts
+++ product-service/handlers/get-products-by-id.ts
@@ -4,6 +4,12 @@ import { ProductsService } from '../services/products-service';
 
 
 export const getProductsById: APIGatewayProxyHandler = async (_event, _context) => {
+  const headers = {
+    "Access-Control-Allow-Headers" : "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS,POST,GET,DELETE"
+  };
+
   try {
     const { productId } = _event.pathParameters;
 
@@ -11,11 +17,13 @@ export const getProductsById: APIGatewayProxyHandler = async (_event, _context) 
 
     return {
       statusCode: 200,
+      headers,
       body: JSON.stringify(productById)
     };
   } catch (e) {
     return {
       statusCode: 404,
+      headers,
       body: JSON.stringify({ error: e })
     }
   }
